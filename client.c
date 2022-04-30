ac5ffdacd0215839229dd1cee67601a1723ae36d
chaeinP <pca0046@gmail.com>
2022-05-01 01:22:54 +0900

    mandatory error handling update

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: chaepark <chaepark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 23:18:01 by chaepark          #+#    #+#             */
-/*   Updated: 2022/04/28 00:31:40 by chaepark         ###   ########.fr       */
+/*   Updated: 2022/05/01 01:22:41 by chaepark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,10 @@ int	main(int argc, char *argv[])
 	char	*message;
 
 	if (argc != 3)
+	{
 		ft_putstr("Bad Request, Invalid Parameters");
+		return (0);
+	}
 	server_pid = ft_atoi(argv[1]);
 	message = argv[2];
 	while (*message)
